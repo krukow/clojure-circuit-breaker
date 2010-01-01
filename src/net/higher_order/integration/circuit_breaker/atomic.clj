1dfed6154d5892d5be3fc0f83ce9cc97d0508f7f
Karl Krukow <karl.krukow@gmail.com>
2010-01-01 17:48:15 +0100

    Updated namespace to .net

---
+++ src/net/higher_order/integration/circuit_breaker/atomic.clj
@@ -0,0 +1,79 @@
+(comment
+  "Implementation of Michael Nygaard's Circuit breaker stability pattern.
+
+A circuit breaker acts as an intemediary between a caller and a callee.
+Typically the callee is an interface to an integration point, and
+the caller is a client of that integration point.
+
+If the circuit breaker detects a number of failures in the callee it 
+'trips' (i.e., transitions to the open state) and prevents further 
+calls to the callee for a fixed period (fail fast).
+
+After this period it lets a single call go though and transitions
+to the initial state or back to the open state depending on the outcome.")
+
+(ns net.higher-order.integration.circuit-breaker.atomic
+  (:use net.higher-order.integration.circuit-breaker.states))
+
+(def default-policy (TransitionPolicy 5 5000))
+(def initial-state (ClosedState default-policy 0))
+
+(defn make-circuit-breaker 
+  ([] (atom initial-state))
+  ([s] (atom s)))
+
+(def transition-by!)
+
+(defn wrap-with [f state]
+  (fn [& args]
+    (let [s (transition-by! on-before-call state)]
+      (if (proceed s)
+	(try
+	 (let [res (apply f args)]
+	   (do (transition-by! on-success state)
+	       res))
+	 (catch Exception e
+	   (do
+	     (transition-by! on-error state)
+	     (throw e))))
+	(throw (RuntimeException. "OpenCircuit"))))))
+
+(defn wrap [f]
+  (let [state (make-circuit-breaker)]
+    [(wrap-with f state) state]))
+
+(defn transition-by! [f state]
+  (loop [s @state
+	 t (f s)]
+    (if (or (identical? s t) 
+	    (compare-and-set! state s t))
+      t
+      (let [s1 @state]
+	(recur s1 (f s1))))))
+
+(comment 
+  test
+  
+  (let [[sf st]  (wrap (constantly 42))]
+    (def #^{:private true}s sf)
+    (def #^{:private true}f (wrap-with (fn [] (throw (Exception.))) st))
+    (def state st))
+  
+  (dotimes [i 10]
+    (s))
+  
+  (assert (= (ClosedState default-policy 0) @state))
+  
+  (dotimes [i 5]
+    (try (f) (catch Exception e)))
+  
+  (assert (= (ClosedState default-policy 5) @state))
+  
+  (try (f) (catch Exception e))
+  
+  (assert (= (class (OpenState default-policy 0)) (class @state)))
+  (Thread/sleep 5000)
+  (s)
+  (assert (= (ClosedState default-policy 0) @state))
+
+)
