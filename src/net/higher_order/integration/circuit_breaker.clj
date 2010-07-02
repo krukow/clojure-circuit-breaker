84199a568ad02ed73c87b4330221641c1187a8d4
Karl Krukow <karl.krukow@gmail.com>
2010-07-02 12:30:52 +0200

    removed old files

--- src/net/higher_order/integration/circuit_breaker.clj
+++
@@ -1,6 +0,0 @@
-(ns net.higher-order.integration.circuit-breaker)
-
-(defprotocol CircuitBreaker
-  (#^clojure.lang.IFn wrap [this #^clojure.lang.IFn fn])
-  (#^net.higher_order.integration.circuit_breaker.states.CircuitBreakerTransitions current-state [this]))
-  
